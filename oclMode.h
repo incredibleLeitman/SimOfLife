4bde5f208af3a3be039f6570ffb2935e32d22886
incredibleLeitman <michael.leithner@gmail.com>
2020-11-11 21:32:01 +0100

    adding output for debug and mode selection

--- oclMode.h
+++ oclMode.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include "common.h"
+
 void runOCL(const char* fileI, const char* fileO, unsigned int generations, unsigned int platformId, unsigned int deviceId)
 {
+#ifdef _DEBUG
+    std::cout << "DEBUG" << std::endl;
+#endif
+    std::cout << "running mode: ocl" << std::endl;
+
     // init grid from file
     Timing::getInstance()->startSetup();
     // TODO: fill datastructure
