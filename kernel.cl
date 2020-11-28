2f3faaba0c4d0b869eadad23f03008a7eee8a246
incredibleLeitman <michael.leithner@gmail.com>
2020-11-28 23:31:37 +0100

    first working draft for openCL (x64) compilation

--- kernel.cl
+++ kernel.cl
@@ -2,17 +2,17 @@
 /*
 * a kernel that simply prints the (2-dimensional) thread id
 * processes no data
-*/
+*
 __kernel void hello(void)
 {
 	int tidX = get_global_id(0);
 	int tidY = get_global_id(1);
 	printf("hi from thread (%d,%d) \n", tidX, tidY);
-}
+}*/
 
 /*
 * a kernel that add the elements of two vectors pairwise
-*/
+*
 __kernel void vector_add(
 	__global const int *A,
 	__global const int *B,
@@ -20,4 +20,90 @@ __kernel void vector_add(
 {
 	int i = get_global_id(0);
 	C[i] = A[i] + B[i];
+}*/
+
+/*
+* a kernel for game of life generation
+*/
+void kernel gol_generation(global int* board, global int* tempBoard, int useTemp, int width, int height)
+{
+    int id = get_global_id(0);
+    int x = id % width;
+    int y = id / width;
+
+    int y_ = y - 1;
+    y_ += (y_ < 0) * height;
+    int x_ = x - 1;
+    x_ += (x_ < 0) * width;
+    int y1 = y + 1;
+    y1 -= (y1 >= height) * height;
+    int x1 = x + 1;
+    x1 -= (x1 >= width) * width;
+
+    if (useTemp > 0)
+    {
+        int livingNeighbors = tempBoard[id];
+        livingNeighbors += tempBoard[x_ + width * y_];
+        livingNeighbors += tempBoard[x + width * y_];
+        livingNeighbors += tempBoard[x1 + width * y_];
+
+        livingNeighbors += tempBoard[x_ + width * y];
+        livingNeighbors += tempBoard[x1 + width * y];
+
+        livingNeighbors += tempBoard[x_ + width * y1];
+        livingNeighbors += tempBoard[x + width * y1];
+        livingNeighbors += tempBoard[x1 + width * y1];
+
+        board[id] = (livingNeighbors == 3 + tempBoard[id] * (livingNeighbors == 4)) > 0;
+    }
+    else
+    {
+        int livingNeighbors = board[id];
+        livingNeighbors += board[x_ + width * y_];
+        livingNeighbors += board[x + width * y_];
+        livingNeighbors += board[x1 + width * y_];
+
+        livingNeighbors += board[x_ + width * y];
+        livingNeighbors += board[x1 + width * y];
+
+        livingNeighbors += board[x_ + width * y1];
+        livingNeighbors += board[x + width * y1];
+        livingNeighbors += board[x1 + width * y1];
+
+        tempBoard[id] = (livingNeighbors == 3 + board[id] * (livingNeighbors == 4)) > 0;
+    }
+
+    /*
+    // need to get current neighbour count, because other than seqMode updates are not diffs but full states
+    // first handle all cells without border mapping, afterwards special handling
+    for (row = 0; row < height; row++)
+    {
+        yOffTop = (row == 0) ? col_bot : -(int)w;
+        yOffBot = (row == (h - 1)) ? -col_bot : w;
+
+        for (col = 1; col < col_right; col++)
+        {
+            idx = col + (row * w); // fastest way
+            *(neighbours + idx) = sumNeighbours(cells + idx, yOffTop, yOffBot, xOffLeft, xOffRight);
+        }
+
+        // handle border for x == 0
+        idx = (row * w);
+        *(neighbours + idx) = sumNeighbours(cells + idx, yOffTop, yOffBot, col_right, xOffRight);
+
+        // handle border for x == col_right
+        idx = col_right + (row * w);
+        *(neighbours + idx) = sumNeighbours(cells + idx, yOffTop, yOffBot, xOffLeft, -col_right);
+    }
+
+    // change cells dependent on oldCells
+    for (idx = 0; idx < elems; ++idx)
+    {
+        value = *(cells + idx);
+        countNeighbours = *(neighbours + idx);
+
+        // alternative option: always set value
+        * (cells + idx) = (countNeighbours == 3) + value * (countNeighbours == 2);
+    }
+    */
 }
--- oclMode.h
+++ oclMode.h
@@ -11,9 +11,94 @@
 // - cl.hpp, the C++ bindings for OpenCL v 1.2
 #include <CL/cl.hpp>
 
-void initOCL(unsigned int plaformId, unsigned int deviceId)
+const std::string KERNEL_FILE = "kernel.cl";
+
+
+cl::Buffer boardBuffer;
+cl::Buffer tempBoardBuffer;
+cl::Kernel kernel;
+cl::CommandQueue queue;
+
+void oclReadFromFile(const char* filePath)
 {
-	const std::string KERNEL_FILE = "kernel.cl";
+	std::cout << "read file: " << filePath << "..." << std::endl;
+	std::ifstream in(filePath);
+	if (in.is_open())
+	{
+		std::string line;
+		std::getline(in, line); // get first line for explizit w / h
+
+		// TODO: better split
+		size_t pos = line.find(',');
+		if (pos != std::string::npos)
+		{
+			w = std::stoi(line.substr(0, pos));
+			h = std::stoi(line.substr(pos + 1));
+		}
+		else // use default values
+		{
+			w = 1000;
+			h = 10000;
+		}
+
+		total_elem_count = w * h;
+		col_right = w - 1;
+		col_bot = total_elem_count - w;
+		row_bot = h - 1;
+		std::cout << "total: " << total_elem_count << ", w: " << w << ", h: " << h << std::endl;
+		std::cout << "col_right: " << col_right << ", col_bot: " << col_bot << ", row_bot: " << row_bot << std::endl;
+
+		cells = new unsigned char[total_elem_count];
+		memset(cells, 0, total_elem_count);
+
+		unsigned int idx = 0;
+		char c;
+		for (unsigned int y = 0; y < h; y++)
+		{
+			getline(in, line);
+			for (unsigned int x = 0; x < w; x++)
+			{
+				c = line[x];
+				//std::cout << "read c: " << c << " for index: " << idx << std::endl;
+				if (c == 'x')  // only need to set alive cells, otherwise stay 0
+				{
+					*(cells + idx) = STATE_ALIVE;
+				}
+
+				if (++idx >= total_elem_count) break;
+			}
+		}
+	}
+	else std::cout << "Error opening " << filePath << std::endl;
+
+	if (!in.eof() && in.fail())
+		std::cout << "error reading " << filePath << std::endl;
+
+	in.close();
+}
+
+void oclWriteToFile(const char* filePath, bool drawNeighbours = false)
+{
+	std::cout << "write file: " << filePath << "..." << std::endl;
+	std::ofstream out(filePath);
+	if (out.is_open())
+	{
+		out << w << "," << h << std::endl;
+		for (unsigned int i = 0; i < total_elem_count; ++i)
+		{
+			if (drawNeighbours) out << (neighbours[i] + 0);
+			else out << ((cells[i] & STATE_ALIVE) ? "x" : ".");
+
+			if (i % w == w - 1) out << std::endl;
+		}
+	}
+	else std::cout << "Error opening " << filePath << std::endl;
+
+	out.close();
+}
+
+void initOCL(unsigned int platformId, unsigned int deviceId)
+{
 	cl_int err = CL_SUCCESS;
 	cl::Program program;
 	std::vector<cl::Device> devices;
@@ -23,38 +108,41 @@ void initOCL(unsigned int plaformId, unsigned int deviceId)
 		// get available platforms ( NVIDIA, Intel, AMD,...)
 		std::vector<cl::Platform> platforms;
 		cl::Platform::get(&platforms);
-		if (platforms.size() == 0 || platforms.size() < plaformId) throw "specified OpenCL platform not available!\n";
+		if (platforms.size() == 0 || platforms.size() < platformId) throw "specified OpenCL platform not available!\n";
 
-		bool test = true;
+#ifdef _DEBUG
 		// test output to gather information about installed hardware:
 		// SyntaX-Desktop:
 		//	platform: NVIDIA CUDA
 		//		device: GeForce GTX 1080
 		//	platform: Intel(R) OpenCL HD Graphics
 		//		device : Intel(R) HD Graphics 530
-		if (test)
+		for (auto& platform : platforms)
 		{
-			for (auto& platform : platforms)
+			std::cout << "platform: " << platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+			//cl_context_properties properties[] = { CL_CONTEXT_PLATFORM, (cl_context_properties)(platform)(), 0 };
+			//cl::Context context(CL_DEVICE_TYPE_ALL, properties);
+			//devices = context.getInfo<CL_CONTEXT_DEVICES>();
+			platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+			for (auto& device : devices)
 			{
-				std::cout << "platform: " << platform.getInfo<CL_PLATFORM_NAME>() << "\n";
-				cl_context_properties properties[] = { CL_CONTEXT_PLATFORM, (cl_context_properties)(platform)(), 0 };
-				cl::Context context(CL_DEVICE_TYPE_ALL, properties);
-				devices = context.getInfo<CL_CONTEXT_DEVICES>();
-				for (auto& device : devices)
-				{
-					std::cout << "device: " << device.getInfo<CL_DEVICE_NAME>() << "\n";
-				}
+				std::cout << "device: " << device.getInfo<CL_DEVICE_NAME>() << "\n";
 			}
 		}
+#endif
 
 		// create a context and get available devices
+		cl::Platform platform = platforms[platformId];
+		std::cout << "using platform: " << platform.getInfo<CL_PLATFORM_NAME>() << "\n";
 
-		cl::Platform platform = platforms[plaformId];
-		cl_context_properties properties[] = { CL_CONTEXT_PLATFORM, (cl_context_properties)(platform)(), 0 };
-		cl::Context context(CL_DEVICE_TYPE_GPU, properties);
-
-		devices = context.getInfo<CL_CONTEXT_DEVICES>();
+		platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 		if (devices.size() == 0 || devices.size() < deviceId) throw "specified OpenCL device not available!\n";
+
+		cl::Device device = devices[deviceId];
+		std::cout << "using device: " << device.getInfo<CL_DEVICE_NAME>() << "\n";
+
+		cl::Context context({ device });
+		cl::Program::Sources sources;
 
 		// load and build the kernel
 		std::ifstream sourceFile(KERNEL_FILE);
@@ -65,64 +153,19 @@ void initOCL(unsigned int plaformId, unsigned int deviceId)
 			(std::istreambuf_iterator<char>()));
 		cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
 		program = cl::Program(context, source);
-		program.build(devices);
+		//program.build(devices);
+		if (program.build({ device }) != CL_SUCCESS) std::cerr << " Error building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
 
-		//create kernels
-		cl::Kernel kernel(program, "hello", &err);
-		cl::Event event;
+		// init buffer and Kernel
+		boardBuffer = cl::Buffer(context, CL_MEM_READ_WRITE, sizeof(int) * total_elem_count);
+		tempBoardBuffer = cl::Buffer(context, CL_MEM_READ_WRITE, sizeof(int) * total_elem_count);
+		
+		kernel = cl::Kernel(program, "gol_generation");
 
-		// launch hello kernel
-		std::cout << "calling 'hello' kernel" << std::endl;
-		cl::CommandQueue queue(context, devices[deviceId], 0, &err);
-		queue.enqueueNDRangeKernel(
-			kernel,
-			cl::NullRange,
-			cl::NDRange(4, 4),
-			cl::NullRange,
-			NULL,
-			&event);
-		event.wait();
+		queue = cl::CommandQueue(context, device);
+		queue.enqueueWriteBuffer(boardBuffer, CL_TRUE, 0, sizeof(unsigned char) * total_elem_count, cells);
+		queue.enqueueWriteBuffer(tempBoardBuffer, CL_TRUE, 0, sizeof(int) * total_elem_count, neighbours);
 
-		// create input and output data
-		std::vector<int> a({ 1,2,3,4,5,6,7,8 });
-		std::vector<int> b({ 1,2,3,4,5,6,7,8 });
-		std::vector<int> c;
-		c.resize(a.size());
-		// input buffers
-		cl::Buffer bufferA = cl::Buffer(context, CL_MEM_READ_ONLY, a.size() * sizeof(int));
-		cl::Buffer bufferB = cl::Buffer(context, CL_MEM_READ_ONLY, b.size() * sizeof(int));
-		// output buffers
-		cl::Buffer bufferC = cl::Buffer(context, CL_MEM_WRITE_ONLY, c.size() * sizeof(int));
-
-		// fill buffers
-		queue.enqueueWriteBuffer(
-			bufferA, // which buffer to write to
-			CL_TRUE, // block until command is complete
-			0, // offset
-			a.size() * sizeof(int), // size of write 
-			&a[0]); // pointer to input
-		queue.enqueueWriteBuffer(bufferB, CL_TRUE, 0, b.size() * sizeof(int), &b[0]);
-
-		cl::Kernel addKernel(program, "vector_add", &err);
-		addKernel.setArg(0, bufferA);
-		addKernel.setArg(1, bufferB);
-		addKernel.setArg(2, bufferC);
-
-		// launch add kernel
-		// Run the kernel on specific ND range
-		cl::NDRange global(a.size());
-		cl::NDRange local(1); //make sure local range is divisible by global range
-		cl::NDRange offset(0);
-		std::cout << "call 'vector_add' kernel" << std::endl;
-		queue.enqueueNDRangeKernel(addKernel, offset, global, local);
-
-		// read back result
-		queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, c.size() * sizeof(int), &c[0]);
-
-		for (int i : c)
-		{
-			std::cout << i << std::endl;
-		}
 	}
 	catch (cl::Error err)
 	{
@@ -142,14 +185,38 @@ void runOCL(const char* fileI, const char* fileO, unsigned int generations, unsi
 #endif
 	std::cout << "running mode: ocl" << std::endl;
 
-	Timing::getInstance()->startSetup();
 	// init grid from file
-	// TODO: fill datastructure
+	Timing::getInstance()->startSetup();
+	ompReadFromFile(fileI);
+
+	// make an array for saving neighbour count for each cell
+	neighbours = new int[total_elem_count];
 
 	initOCL(platformId, deviceId);
+
+	bool useTemp = false;
+	unsigned int gen = 0;
 	Timing::getInstance()->stopSetup();
 
 	Timing::getInstance()->startComputation();
-	// TODO: do stuff
+	for (gen = 0; gen < generations; gen++)
+	{
+		kernel.setArg(0, boardBuffer);
+		kernel.setArg(1, tempBoardBuffer);
+		kernel.setArg(2, cl_int(useTemp));
+		kernel.setArg(3, cl_int(w));
+		kernel.setArg(4, cl_int(h));
+		queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(total_elem_count), cl::NullRange);
+		queue.finish();
+	}
+	// read back current board state
+	//queue.enqueueReadBuffer(boardBuffer, CL_TRUE, 0, sizeof(int) * total_elem_count, cells);
+	queue.enqueueReadBuffer(boardBuffer, CL_TRUE, 0, sizeof(unsigned char) * total_elem_count, cells);
 	Timing::getInstance()->stopComputation();
+
+	// write out result
+	Timing::getInstance()->startFinalization();
+	oclWriteToFile(fileO);
+	//writeToFile(fileO, true); // writes count of neighbours instead of just x / .
+	Timing::getInstance()->stopFinalization();
 }
